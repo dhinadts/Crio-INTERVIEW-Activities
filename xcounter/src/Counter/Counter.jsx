5e0d817019c85d2a2d95afc05f34f5c73113b536
Dhinakaran K <dhinadts@gmail.com>
2025-06-07 01:47:53 +0530

    H1 tag for heading

--- xcounter/src/Counter/Counter.jsx
+++ xcounter/src/Counter/Counter.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 
-
 const Counter = () => {
-    const [valueOfCount, setCount] = useState(0);
+  const [valueOfCount, setCount] = useState(0);
 
-    const Increment = () => {
-        // count++;
-        setCount(count => count+1);
-    }
+  const Increment = () => {
+    // count++;
+    setCount((count) => count + 1);
+  };
 
-    const Decrement = () => {
-        // count--;
-        setCount(count => count-1);
-    }
+  const Decrement = () => {
+    // count--;
+    setCount((count) => count - 1);
+  };
 
-    return (<>
-    <div>Counetr APP</div>
-    <div>count: {valueOfCount}</div>
-    <div>
-        <button type="button" onClick={Increment}>Increment</button>
-        <button type="button" onClick={Decrement}>Decrement</button>
-    </div>
-    </>);
-}
+  return (
+    <>
+      <div>
+        <h1>Counetr APP</h1>
+      </div>
+      <div>count: {valueOfCount}</div>
+      <div>
+        <button type="button" onClick={Increment}>
+          Increment
+        </button>
+        <button type="button" onClick={Decrement}>
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Counter;
