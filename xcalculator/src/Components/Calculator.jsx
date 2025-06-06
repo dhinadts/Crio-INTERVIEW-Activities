a485e2f4e00a4c92973e024b40c70d62d3cb759d
Dhinakaran K <dhinadts@gmail.com>
2025-06-07 03:29:15 +0530

    Removed unwanted lines

--- xcalculator/src/Components/Calculator.jsx
+++ xcalculator/src/Components/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
 import { evaluate } from "mathjs";
 const Calculator = () => {
