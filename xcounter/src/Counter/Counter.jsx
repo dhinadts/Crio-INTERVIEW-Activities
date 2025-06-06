aaa44d6664b88fef9416ee86246edf91423b20be
Dhinakaran K <dhinadts@gmail.com>
2025-06-07 01:42:30 +0530

    XCOUNTER

---
+++ xcounter/src/Counter/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+
+const Counter = () => {
+    const [valueOfCount, setCount] = useState(0);
+
+    const Increment = () => {
+        // count++;
+        setCount(count => count+1);
+    }
+
+    const Decrement = () => {
+        // count--;
+        setCount(count => count-1);
+    }
+
+    return (<>
+    <div>Counetr APP</div>
+    <div>count: {valueOfCount}</div>
+    <div>
+        <button type="button" onClick={Increment}>Increment</button>
+        <button type="button" onClick={Decrement}>Decrement</button>
+    </div>
+    </>);
+}
+
+export default Counter;
