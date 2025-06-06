5d9e50c355ee38a872aaf686a6ecb905e1523cc8
Dhinakaran K <dhinadts@gmail.com>
2025-06-06 18:28:28 +0530

    UI Value removed

--- xexpensetracker/src/Components/Transactions.jsx
+++ xexpensetracker/src/Components/Transactions.jsx
@@ -22,7 +22,7 @@ const Transactions = () => {
         <div className="trans-card-container">
           {items.length > 0 ? (
             items.map((e) => (
-              <div>
+              <div className="card-details">
                 <div className="list-tile" key={e.title}>
                   <div className="left-section">
                     <MdEmojiFoodBeverage size={24} color={e.color} />
@@ -31,7 +31,7 @@ const Transactions = () => {
                         {e.title}
                       </span>
                       <span name="category" style={{ fontWeight: "200" }}>
-                        {e.category} {e.date}
+                        {/* {e.category}  */}{e.date}
                       </span>
                     </div>
                   </div>
@@ -68,12 +68,12 @@ const Transactions = () => {
               </div>
             ))
           ) : (
-            <div className="content">No transactions!</div>
+            <div className="card-details content">No transactions!</div>
           )}
         </div>
       </div>
 
-      <div className="card-wrapper">
+      <div className="card-wrapper ">
         <h2 className="section-heading">Top Expenses</h2>
         <div className="trans-card-container-second topExpenses">
           <div className="codeIndication">
