e8e206c590c8367ffc772a5dfdb91ea5dd1f1d3d
Dhinakaran K <dhinadts@gmail.com>
2025-06-29 10:22:33 +0530

    xSpellCheck

---
+++ xspellcheck/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import SpellCheckApp from "./SpellCheckApp";
+
+
+function App() {
+  return (
+    <div className="App">
+          <SpellCheckApp />
+    </div>
+  );
+}
+
+export default App;
