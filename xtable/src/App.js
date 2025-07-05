0eb3483493604c5b0f61c60d3c66f441b8aabe42
Dhinakaran K <dhinadts@gmail.com>
2025-07-05 23:44:53 +0530

    XTable

---
+++ xtable/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import XTable from './XTable';
+
+function App() {
+  return (
+    <div >
+     <XTable />
+    </div>
+  );
+}
+
+export default App;
